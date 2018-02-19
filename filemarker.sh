150ec969c209858ae9ddc79d068f03aa2a6563c5
jshields <shields.h.josh@gmail.com>
2018-02-19 17:11:50 -0600

    fix pre-commit hook file

--- filemarker.sh
+++ filemarker.sh
@@ -1,14 +1,18 @@
 #!/bin/bash
 
+STATUS=0
+
 # Insert filemarkers to satisfy Apache license
 PYFILES="$(find . -type f -name '*.py')"
 for FILENAME in $PYFILES; do
     # if file doesn't already contain the file marker
     if ! grep -q "Modified by Joshua Shields" $FILENAME; then
-        echo "Marked $FILENAME"
+        # mark file and set status to stop the commit
         printf "\n# Modified by Joshua Shields\n" >> $FILENAME
+        echo "Marked $FILENAME"
+        STATUS=1
     fi
 done
 
-echo "Pre-commit hook executed"
-exit 1
+echo "Pre-commit hook exiting with status $STATUS"
+exit $STATUS
